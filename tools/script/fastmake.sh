6c6dcd6ebae092eb155e0479ddf6c32452123150
Éric Poirier <poirier.eric.1@gmail.com>
2020-10-10 11:53:46 -0400

    Changed to debug build so that debugging is possible, since this is used only in developpement

--- tools/script/fastmake.sh
+++ tools/script/fastmake.sh
@@ -11,9 +11,9 @@
 
 MAKE=make
 
-${MAKE} NDEBUG=1 cxinv/libcxinv.a
-${MAKE} NDEBUG=1 cxlog/libcxlog.a
-${MAKE} NDEBUG=1 cxmodel/libcxmodel.a
-${MAKE} NDEBUG=1 cxgui/libcxgui.a
-${MAKE} NDEBUG=1 cxexec/libcxexec.a
-${MAKE} NDEBUG=1 connectx/connectx
+${MAKE} cxinv/libcxinv.a
+${MAKE} cxlog/libcxlog.a
+${MAKE} cxmodel/libcxmodel.a
+${MAKE} cxgui/libcxgui.a
+${MAKE} cxexec/libcxexec.a
+${MAKE} connectx/connectx
