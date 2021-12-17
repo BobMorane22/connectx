18fb20e56cb6afbbd5b409fc28a358eaa72504d3
BobMorane <eric.poirier7@usherbrooke.ca>
2021-12-17 14:10:59 -0500

    Valgrind script parametrized.
    The script now takes an argument, which is the program to run.

--- tools/script/valgrind.sh
+++ tools/script/valgrind.sh
@@ -3,10 +3,12 @@
 #---------------------------------------------------------------------------------------------------
 # This script launches valgrind on Connect X.
 #
+# Parameters:
+#   $1 : The program to execute under valgring.
+#
 # @file valgrind.sh
 # @date 2021
 #
 #---------------------------------------------------------------------------------------------------
 
-make
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=./valgrind-out.txt ./connectx/connectx
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=./valgrind-out.txt $1
