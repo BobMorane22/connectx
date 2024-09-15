4624450923cdebff73a6c12bd4905bbb017054de
BobMorane <eric.poirier7@usherbrooke.ca>
2024-09-15 11:00:59 -0400

    TG-380 Prototype code removed.
     - The abstract factory prototype code was removed.
     - The hello world example has been modified to build. A target
       hello world example file has been added. It is not mentionned
       in the CMakeLists.txt file so it does not build, but it can
       be used to diff with the actual example to make sure we are
       migrating towards the wanted state of things.

--- doxygen/awt/helloworld_target.cpp
+++ doxygen/awt/helloworld_target.cpp
@@ -15,27 +15,26 @@
  *  along with Connect X. If not, see <https://www.gnu.org/licenses/>.
  *
  *************************************************************************************************/
-/**************************************************************************************************
- * Hello World example using the AWT.
+/**********************************************************************************************//**
+ * This is the target for the AWT hello world example. To be replaced when all the work will be
+ * completed. When completed, the current example should match the code here, and this file
+ * should be removed.
  *
- * @file helloworld.cpp
+ * @file helloworld_target.cpp
  * @date 2024
  *
  *************************************************************************************************/
 
-//! \noop [AWT - Hello World]
+//! \noop [AWT - Hello World Target]
 #include <cstdlib>
 #include <iostream>
 
-#include <gtkmm/application.h>
-
 #include <cxinv/assertion.h>
-#include <cxmodel/common.h>
 #include <cxcmnui/IAbstractWidgetsFactory.h>
 #include <cxcmnui/IButton.h>
 #include <cxcmnui/ILayout.h>
 #include <cxcmnui/ISignal.h>
-#include <cxcmnuigtkmm3/AbstractWidgetsFactory.h>
+#include <cxcmnui/WidgetsToolkit.h>
 
 int main(int argc, char *argv[])
 {
@@ -45,30 +44,35 @@ int main(int argc, char *argv[])
     const auto gtkApplication = Gtk::Application::create(argc, argv);
     IF_CONDITION_NOT_MET_DO(bool(gtkApplication), return EXIT_FAILURE;);
 
-    const std::unique_ptr<IAbstractWidgetsFactory> factory = gtkmm3::CreateFactory(gtkApplication);
+    const std::unique_ptr<IAbstractWidgetsFactory> factory = CreateFactory(argc, argv, WidgetsToolkit::GTKMM3);
     IF_CONDITION_NOT_MET_DO(factory, return EXIT_FAILURE;);
+
+    std::unique_ptr<IWindow> mainWindow = factory->CreateWindow();
+    IF_CONDITION_NOT_MET_DO(mainWindow, return EXIT_FAILURE;);
 
     std::unique_ptr<ILayout> mainLayout = factory->CreateLayout();
     IF_CONDITION_NOT_MET_DO(mainLayout, return EXIT_FAILURE;);
 
-    //! \noop [AWT - Create button]
+    mainWindow->RegisterLayout(*mainLayout);
+
+    //! \noop [AWT - Create button target]
     std::unique_ptr<IButton> button = factory->CreateButton("Hello World!");
-    //! \noop [AWT - Create button]
+    //! \noop [AWT - Create button target]
     IF_CONDITION_NOT_MET_DO(button, return EXIT_FAILURE;);
 
-    //! \noop [AWT - Connecting to a signal]
+    //! \noop [AWT - Connecting to a signal target]
     button->OnClicked()->Connect(
         []()
         {
             std::cout << "Hello World!\n";
         });
-    //! \noop [AWT - Connecting to a signal]
+    //! \noop [AWT - Connecting to a signal target]
 
     mainLayout->Register(
         *button,
-        {cx::model::Row{0u}, ILayout::RowSpan{1u}},
-        {cx::model::Column{0u}, ILayout::ColumnSpan{1u}});
+        {ILayout::Row{0u}, ILayout::RowSpan{1u}},
+        {ILayout::Column{0u}, ILayout::ColumnSpan{1u}});
 
-    return EXIT_SUCCESS;
+    return mainWindow->Show();
 }
-//! \noop [AWT - Hello World]
+//! \noop [AWT - Hello world target]
